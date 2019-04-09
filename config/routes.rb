c1b1c573dd26174cadc8bbeb9a53b6d3a0667d86
Priscila Aranha <aranhaqg@gmail.com>
2019-04-09 15:14:17 -0300

    Add Simple Token Authentication to api.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
+  devise_for :users
 	namespace :api do
     namespace :v1, defaults: { format: :json } do
 		  post 'transactions/transfer'
 		  get 'transactions/balance'
+		  resources :sessions, only: [:create, :destroy]
 		end
 	end
-  devise_for :users, controllers: { sessions: 'sessions'}
 end
